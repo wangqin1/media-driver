d55a508d74ba6aff4bf9c174d2eafc29c35b45c6
Wang Dylan <dylan.wang@intel.com>
2020-09-17 14:04:30 +0800

    [Media]Implemented dynamic Gpu session priority
    Implemented the media and vp Gpu context priority setting.
    
    Signed-off-by: Wang Dylan <dylan.wang@intel.com>

--- media_driver/linux/common/codec/ddi/media_ddi_encode_base.cpp
+++ media_driver/linux/common/codec/ddi/media_ddi_encode_base.cpp
@@ -1243,6 +1243,19 @@ VAStatus DdiEncodeBase::CreateBuffer(
         }
         break;
     }
+#if VA_CHECK_VERSION(1, 10, 0)
+    case VAContextParameterUpdateBufferType:
+    {
+        bufSize       = size;
+        if (bufSize < sizeof(VAContextParameterUpdateBuffer))
+        {
+            va = VA_STATUS_ERROR_INVALID_PARAMETER;
+            CleanUpBufferandReturn(buf);
+            return va;
+        }
+        break;
+    }
+#endif
     default:
     {
         bufSize = size * elementsNum;
